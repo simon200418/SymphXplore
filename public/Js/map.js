6428429f826430d29c8a87783a913c17ba250751
simon200418 <simonpathre20@gmail.com>
2025-03-13 00:53:36 +0530

    Changes to map

--- public/Js/map.js
+++ public/Js/map.js
@@ -3,6 +3,7 @@
 
       const map = new mapboxgl.Map({
             container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
             center: listing.geometry.coordinates , // starting position [lng, lat]. Note that lat must be set between -90 and 90
             zoom: 10 // starting zoom
         });
