648429f669bd3ef6fce8b55b6b6bdfed9184b9b5
simon200418 <simonpathre20@gmail.com>
2025-03-12 21:03:23 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/SymphXplore";
+
 
 const dbUrl = process.env.ATLASDB_URI;
 
